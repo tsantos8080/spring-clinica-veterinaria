588e2c75c82b362c14fc9fea67963913d3ac3834
tsantos8080 <tsantos8080@gmail.com>
2020-09-25 14:51:19 -0300

    refatorado para common crud interface. Closes #11

--- clinica-veterinaria-data/src/main/java/br/tsantos8080/clinicaveterinaria/services/OwnerService.java
+++ clinica-veterinaria-data/src/main/java/br/tsantos8080/clinicaveterinaria/services/OwnerService.java
@@ -2,14 +2,6 @@ package br.tsantos8080.clinicaveterinaria.services;
 
 import br.tsantos8080.clinicaveterinaria.models.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- clinica-veterinaria-data/src/main/java/br/tsantos8080/clinicaveterinaria/services/PetService.java
+++ clinica-veterinaria-data/src/main/java/br/tsantos8080/clinicaveterinaria/services/PetService.java
@@ -2,13 +2,5 @@ package br.tsantos8080.clinicaveterinaria.services;
 
 import br.tsantos8080.clinicaveterinaria.models.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
