f74076669731fcf5ab16679124f43d8f579185c8
tsantos8080 <tsantos8080@gmail.com>
2020-09-25 15:20:54 -0300

    Implementado map based services. Closes #6

---
+++ clinica-veterinaria-data/src/main/java/br/tsantos8080/clinicaveterinaria/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package br.tsantos8080.clinicaveterinaria.services.map;
+
+import br.tsantos8080.clinicaveterinaria.models.Owner;
+import br.tsantos8080.clinicaveterinaria.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+}
---
+++ clinica-veterinaria-data/src/main/java/br/tsantos8080/clinicaveterinaria/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package br.tsantos8080.clinicaveterinaria.services.map;
+
+import br.tsantos8080.clinicaveterinaria.models.Vet;
+import br.tsantos8080.clinicaveterinaria.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+}
