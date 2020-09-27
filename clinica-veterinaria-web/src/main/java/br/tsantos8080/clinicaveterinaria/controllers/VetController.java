3e7c5d496b0f00255d48e0d551685b711de6b701
tsantos8080 <tsantos8080@gmail.com>
2020-09-27 16:32:08 -0300

    Adicionada lista de vets e atualizado controller. Closes #15

--- clinica-veterinaria-web/src/main/java/br/tsantos8080/clinicaveterinaria/controllers/VetController.java
+++ clinica-veterinaria-web/src/main/java/br/tsantos8080/clinicaveterinaria/controllers/VetController.java
@@ -1,13 +1,21 @@
 package br.tsantos8080.clinicaveterinaria.controllers;
 
+import br.tsantos8080.clinicaveterinaria.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
